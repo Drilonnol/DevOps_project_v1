df0ddb7ddea1bf99ba87f0cc5b6e943fff0d91c8
Drilonnol <drilonsadikaj67304@gmail.com>
2026-07-27 20:29:35 +0200

    feat: configure GitHub OIDC authentication for AWS

--- Infrastructure/Terraform-part-2/outputs.tf
+++ Infrastructure/Terraform-part-2/outputs.tf
@@ -57,3 +57,8 @@ output "configure_kubectl" {
 output "backend_irsa_role_arn" {
   value = module.iam.backend_irsa_role_arn
 }
+
+output "github_actions_role_arn" {
+  description = "IAM Role ARN used by GitHub Actions OIDC"
+  value       = module.iam.github_actions_role_arn
+}
