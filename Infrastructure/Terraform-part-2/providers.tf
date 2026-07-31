b7f3ea1a02a897747a524d6d703bc578bb92b66a
Drilonnol <drilonsadikaj67304@gmail.com>
2026-07-31 21:54:29 +0200

    Configure PostgreSQL user grants and IAM auth setup in Terraform

--- Infrastructure/Terraform-part-2/providers.tf
+++ Infrastructure/Terraform-part-2/providers.tf
@@ -6,25 +6,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    postgresql = {
-      source  = "cyrilgdn/postgresql"
-      version = "~> 1.22.0"
-    }
   }
 }
 
 provider "aws" {
   region = var.region
-}
-
-provider "postgresql" {
-  host            = module.rds.db_instance_address
-  port            = module.rds.db_instance_port
-  database        = var.db_name
-  username        = var.db_username
-  password        = var.db_password
-  sslmode         = "require"
-  connect_timeout = 15
 }
 
 data "aws_caller_identity" "current" {}
--- Infrastructure/Terraform-part-2/rds/main.tf
+++ Infrastructure/Terraform-part-2/rds/main.tf
@@ -54,36 +54,3 @@ resource "aws_db_instance" "this" {
     Name = "${var.cluster_name}-postgres-rds"
   }
 }
-
-resource "postgresql_role" "backend_app" {
-  name  = "backend_app"
-  login = true
-  roles = ["rds_iam"]
-
-  depends_on = [aws_db_instance.this]
-}
-
-resource "postgresql_grant" "schema_public" {
-  database    = aws_db_instance.this.db_name
-  role        = postgresql_role.backend_app.name
-  schema      = "public"
-  object_type = "schema"
-  privileges  = ["CREATE", "USAGE"]
-}
-
-resource "postgresql_grant" "tables_public" {
-  database    = aws_db_instance.this.db_name
-  role        = postgresql_role.backend_app.name
-  schema      = "public"
-  object_type = "table"
-  privileges  = ["ALL"]
-}
-
-resource "postgresql_default_privileges" "default_tables" {
-  database    = aws_db_instance.this.db_name
-  role        = postgresql_role.backend_app.name
-  schema      = "public"
-  owner       = aws_db_instance.this.username
-  object_type = "table"
-  privileges  = ["ALL"]
-}
